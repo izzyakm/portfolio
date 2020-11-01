50e53089d76811b26434c0ff56103d7fc5d49198
Izzy AKM <izzyakm@Izzys-MacBook-Air.local>
2020-11-01 15:01:55 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$("#contactizzy").submit(function(e) {
+    e.preventDefault();
+  
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+      alert("Thank you!");
+    });
+  });
